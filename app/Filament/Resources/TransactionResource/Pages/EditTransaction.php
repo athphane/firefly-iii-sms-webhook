cc67243ba7c0f653f2e6484b15065293adc35de8
Athfan Khaleel <athphane@gmail.com>
2025-01-04 22:54:42 +0500

    feat: add action to sync a particular stored transaction

--- app/Filament/Resources/TransactionResource/Pages/EditTransaction.php
+++ app/Filament/Resources/TransactionResource/Pages/EditTransaction.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\TransactionResource\Pages;
 
 use App\Filament\Resources\TransactionResource;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
@@ -13,6 +14,14 @@ class EditTransaction extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('sync-transaction')
+                ->icon('heroicon-o-arrow-path')
+                ->color('success')
+                ->hidden(fn($record) => isset($record->firefly_transaction_id))
+                ->action(function ($record) {
+                    $record->process();
+                }),
+
             DeleteAction::make(),
         ];
     }
