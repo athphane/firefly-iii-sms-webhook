4d69dda4d715508e84d1c686fa49ee871bc3011c
Athfan Khaleel <athphane@gmail.com>
2025-01-06 21:15:56 +0500

    feat: add EUR Currency

--- app/Support/FireflyIII/Enums/Currencies.php
+++ app/Support/FireflyIII/Enums/Currencies.php
@@ -6,11 +6,13 @@ enum Currencies: string
 {
     case USD = 'USD';
     case MVR = 'MVR';
+    case EUR = 'EUR';
 
     public function exchangeRate(): float
     {
         return match ($this) {
             self::USD => 15.42,
+            self::EUR => 16.20,
             self::MVR => 1,
         };
     }
