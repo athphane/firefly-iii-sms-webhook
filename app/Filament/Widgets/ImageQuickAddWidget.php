337e4cc58774dce617c588beab586a4a5e4b5013
athphane <athphane@gmail.com>
2025-02-22 01:52:56 +0500

    feat: wrap the thing in a transaction

--- app/Filament/Widgets/ImageQuickAddWidget.php
+++ app/Filament/Widgets/ImageQuickAddWidget.php
@@ -10,6 +10,7 @@ use Filament\Forms\Contracts\HasForms;
 use Filament\Forms\Form;
 use Filament\Notifications\Notification;
 use Filament\Widgets\Widget;
+use Illuminate\Support\Facades\DB;
 use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
 use Spatie\MediaLibrary\MediaCollections\Exceptions\FileDoesNotExist;
 use Spatie\MediaLibrary\MediaCollections\Exceptions\FileIsTooBig;
@@ -47,26 +48,26 @@ class ImageQuickAddWidget extends Widget implements HasForms
     {
         $this->form->validate();
 
-        $transaction = new Transaction();
-        $transaction->save();
+        DB::transaction(function () {
+            $transaction = new Transaction();
+            $transaction->save();
 
+            if (!empty($this->data['receipt'])) {
+                /** @var TemporaryUploadedFile $receipt */
+                $receipt = Arr::flatten($this->data['receipt'])[0];
 
-        if (!empty($this->data['receipt'])) {
-            /** @var TemporaryUploadedFile $receipt */
-            $receipt = Arr::flatten($this->data['receipt'])[0];
+                $transaction->addMedia($receipt)
+                    ->toMediaCollection('receipt');
 
-            $transaction->addMedia($receipt)
-                ->toMediaCollection('receipt');
+                $this->data['receipt'] = null;
+            }
 
-            $this->data['receipt'] = null;
-        }
-
-        $transaction->process();
-
-        Notification::make('success')
-            ->title('Transaction Submitted')
-            ->body('Transaction will be processed shortly.')
-            ->success()
-            ->send();
+            $transaction->process();
+            Notification::make('success')
+                ->title('Transaction Submitted')
+                ->body('Transaction will be processed shortly.')
+                ->success()
+                ->send();
+        });
     }
 }
