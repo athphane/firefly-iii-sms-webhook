f9ff86b1fe68cc930aac2787dbdc8d2f704fd2b8
Athfan Khaleel <athphane@gmail.com>
2025-05-09 01:57:54 +0500

    wip: telegram webhook integration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,7 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->validateCsrfTokens(except: [
-            'webhooks/telegram/*',
+            '/telegram/*/webhook',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
